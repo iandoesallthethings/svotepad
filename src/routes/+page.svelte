e95f3e4afc71005100e25e5d8a47b93e3d2aa506
iandoesallthethings <iandoesallthethings@gmail.com>
2023-09-12 12:48:05 -0400

    Don't require hover for docs.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+	import { browser } from '$app/environment'
 	import documents, { newDocument } from '$lib/Documents'
+	import { onMount } from 'svelte'
+	import Device from 'svelte-device-info'
+
+	$: isTouchDevice = browser ? Device.isMobile : false
 
 	function createDocument() {
 		const name = prompt('Document name')
@@ -18,11 +23,19 @@
 	}
 </script>
 
+<h3 class="text-center font-bold">Documents</h3>
+
+<hr class="my-4" />
+
+{#if $documents.length === 0}
+	<p class="text-center text-gray-500">No documents</p>
+{/if}
+
 {#each $documents as document, index}
 	<a role="button" href={`/${index}`} class="group relative flex items-center justify-center">
 		{document.name}
 
-		<div class="absolute right-0 hidden group-hover:block">
+		<div class:!block={isTouchDevice} class="absolute right-0 hidden group-hover:block">
 			<button on:click|stopPropagation|preventDefault={() => renameDocument(index)}>✏️</button>
 			<button on:click|stopPropagation|preventDefault={() => removeDocument(index)}>🗑️</button>
 		</div>
